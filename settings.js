515ca0deaad2bffb3e948d25e8e8bdd62017dbb0
Adam Howard <medavox@users.noreply.github.com>
2025-08-04 18:50:16 +0100

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,8 @@
+var settings = {
+    title: "My VS Server",
+    titleBarName: "My VS",
+    titleBarCommunity: "My Community",
+    siteUrl: "https://example.com",
+    updateText : "Last updated on ",
+    lastUpdated : "04 August 2025."
+}
